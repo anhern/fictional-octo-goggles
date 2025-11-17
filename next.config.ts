a5bf7f98c50e83c67ce628dc0607ab1d64448f98
Anneliese <anneliesenhernandez@gmail.com>
2025-11-16 20:07:12 -0500

    try redirect function

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,34 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+      async redirects() {
+        return [
+          {
+            source: '/index.html', // The incoming request path pattern
+            destination: '/', // The path to redirect to
+            permanent: true, // true for 308 (permanent), false for 307 (temporary)
+          },
+          // Add more redirect rules as needed
+        ];
+      },
 };
 
 export default nextConfig;
+
+
+//  async redirects() {
+//     return [
+//       // Basic redirect
+//       {
+//         source: '/about',
+//         destination: '/',
+//         permanent: true,
+//       },
+//       // Wildcard path matching
+//       {
+//         source: '/blog/:slug',
+//         destination: '/news/:slug',
+//         permanent: true,
+//       },
+//     ]
+//   },
